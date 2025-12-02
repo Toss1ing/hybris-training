25aac59f89ffc2bb9a662daa74f034dd23b69a3d
Toss1ing <154839710+Toss1ing@users.noreply.github.com>
2025-12-02 18:29:14 +0300

    Create concerttours and concerttickets extensions and create items (#1)
    * Create concerttours and concerttickets extensions and create items
    
    * Refactored text in description tags

---
+++ bin/custom/concerttickets/src/org/training/setup/ConcertticketsSystemSetup.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package org.training.setup;
+
+import static org.training.constants.ConcertticketsConstants.PLATFORM_LOGO_CODE;
+
+import de.hybris.platform.core.initialization.SystemSetup;
+
+import java.io.InputStream;
+
+import org.training.constants.ConcertticketsConstants;
+import org.training.service.ConcertticketsService;
+
+
+@SystemSetup(extension = ConcertticketsConstants.EXTENSIONNAME)
+public class ConcertticketsSystemSetup
+{
+	private final ConcertticketsService concertticketsService;
+
+	public ConcertticketsSystemSetup(final ConcertticketsService concertticketsService)
+	{
+		this.concertticketsService = concertticketsService;
+	}
+
+	@SystemSetup(process = SystemSetup.Process.INIT, type = SystemSetup.Type.ESSENTIAL)
+	public void createEssentialData()
+	{
+		concertticketsService.createLogo(PLATFORM_LOGO_CODE);
+	}
+
+	private InputStream getImageStream()
+	{
+		return ConcertticketsSystemSetup.class.getResourceAsStream("/concerttickets/sap-hybris-platform.png");
+	}
+}
