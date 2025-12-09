c13432a1cd81dbcc8861d90b3e2186218df10b1f
toss1ing <kirill.borisenko05@mail.ru>
2025-12-09 22:29:01 +0300

    added controllers + jsp page for all extensions

--- bin/custom/concerttickets/src/org/training/facades/impl/DefaultTicketFacade.java
+++ bin/custom/concerttickets/src/org/training/facades/impl/DefaultTicketFacade.java
@@ -5,8 +5,8 @@ import org.training.facades.TicketFacade;
 import org.training.model.TicketsModel;
 import org.training.service.TicketService;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DefaultTicketFacade implements TicketFacade {
 
@@ -19,13 +19,9 @@ public class DefaultTicketFacade implements TicketFacade {
     @Override
     public List<TicketData> getTickets() {
         List<TicketsModel> models = ticketService.getTickets();
-        List<TicketData> result = new ArrayList<>();
-
-        for (TicketsModel model : models) {
-            result.add(mapToTicketData(model));
-        }
-
-        return result;
+        return models.stream()
+                .map(this::mapToTicketData)
+                .collect(Collectors.toList());
     }
 
     private TicketData mapToTicketData(final TicketsModel ticket) {
